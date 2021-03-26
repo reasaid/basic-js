26e1d55fa18e98f9dffae56a8db3d4c34bac67ce
Maria Lobanova <roldik95@gmail.com>
2021-03-26 13:50:50 +0300

    feat: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let strCut=str;
+  result = "";
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+  if (repeatTimes === undefined) repeatTimes = 1;
+  if (separator === undefined) separator = "+";
+  if (addition === undefined) addition = "";
+  if (additionRepeatTimes === undefined) additionRepeatTimes = 1;
+  if (additionSeparator === undefined) additionSeparator = "|";
+  for (let i = 0; i < additionRepeatTimes; i++){
+    if (i == additionRepeatTimes-1){
+      strCut += addition + separator;
+    }else{
+      strCut += addition + additionSeparator;
+    }
+  }
+  for (let i = 0; i < repeatTimes; i++){
+    if(i == repeatTimes - 1){
+      result += strCut.substr(0, strCut.length-separator.length);
+    }else{
+      result += strCut;
+    }
   
+  }
+  return result;
+  };
+    
